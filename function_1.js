115922f46efa956e882cf6898c6295ee8aebf335
sakib228 <sakinhossainmd1@gmail.com>
2026-05-30 13:12:20 +0600

    result structure code

--- function_1.js
+++ function_1.js
@@ -3,73 +3,73 @@
 //     return res;
 //     // console.log("the results is : ", res);
 // }
-// let num1 = 20;
-// let num2 = 30;
-// let result = add(num1, num2);
-// console.log("the results is : ", result);
-// // -----------------------------------------------------------------
-// function comp(num3,num4){
-// if(num3 >= num4){
-//     return (num3*num3);
-// }
-// else{
-//     return(num4*2);
-// }
-// }
-// console.log("this is a finding number : ", comp(5,3));
-// console.log("this is a finding number : ", comp(1,3));
-// // -----------------------------------------------------------------
-// function cond(number1, number2){
-//     if(number1 === number2){
-//         // return(number1 + number2);
-//         return true;
-//     }
-//     else{
-//         // return(number1 - number2);
-//         return false;
-//     }
-// }
-// let res = cond(2,4);
-// console.log("the result is : ", res);
+let num1 = 20;
+let num2 = 30;
+let result = add(num1, num2);
+console.log("the results is : ", result);
 // -----------------------------------------------------------------
-// function arr(number){
-//     let res1 = number.length;
-//     console.log("the result is : ", res1);
-//     if(res1 % 2 === 0){
-//         return true;
-//     }
-//     else{
-//        return false;
-//     }
-// }
-// let res1 = arr([3,2,4,5,6,1,4,3,11,3]);
-// console.log("the array length is : ", res1);
+function comp(num3,num4){
+if(num3 >= num4){
+    return (num3*num3);
+}
+else{
+    return(num4*2);
+}
+}
+console.log("this is a finding number : ", comp(5,3));
+console.log("this is a finding number : ", comp(1,3));
+// -----------------------------------------------------------------
+function cond(number1, number2){
+    if(number1 === number2){
+        // return(number1 + number2);
+        return true;
+    }
+    else{
+        // return(number1 - number2);
+        return false;
+    }
+}
+let res = cond(2,4);
+console.log("the result is : ", res);
+// -----------------------------------------------------------------
+function arr(number){
+    let res1 = number.length;
+    console.log("the result is : ", res1);
+    if(res1 % 2 === 0){
+        return true;
+    }
+    else{
+       return false;
+    }
+}
+let res1 = arr([3,2,4,5,6,1,4,3,11,3]);
+console.log("the array length is : ", res1);
 
-// // -----------------------------------------------------------------
-// function check(number){
-//     let res = number.length;
-//     console.log("the number is : ", res);
-//     if(res % 2 !== 1){
-//         return true;
-//     }
-//     else{
-//         return false;
-//     }
-// }
-// let text = check("hello world");
-// console.log("this is the result : ", text);
 // -----------------------------------------------------------------
-// function check(number, checked){
-//     if(checked === true){
-//         const res = number * 2;
-//         return res;
-//     }
-//     else{
-//         const res = number - 1;
-//         return res;
-//     }
-// }
-// const x = check(5,true);
-// console.log("the result is : ", x );
-// const y = check( 6,false);
-// console.log("The result is : ", y);
+function check(number){
+    let res = number.length;
+    console.log("the number is : ", res);
+    if(res % 2 !== 1){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+let text = check("hello world");
+console.log("this is the result : ", text);
+// -----------------------------------------------------------------
+function check(number, checked){
+    if(checked === true){
+        const res = number * 2;
+        return res;
+    }
+    else{
+        const res = number - 1;
+        return res;
+    }
+}
+const x = check(5,true);
+console.log("the result is : ", x );
+const y = check( 6,false);
+console.log("The result is : ", y);
